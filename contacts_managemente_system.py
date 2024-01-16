a0042650be72cc2cbf5867925fc3097329676535
Helena Trigo <trigoh91@gmail.com>
2024-01-16 10:29:12 +0000

    Contacts management system - version 1

---
+++ contacts_managemente_system.py
@@ -0,0 +1,17 @@
+# classe contactos com os campos nome, telefone e email
+class Contact:
+    def __init__(self, nome, telemovel, email):
+        self.name = nome
+        self.mobile: telemovel
+        self.email: email
+
+
+# lista de objetos do tipo contacto
+
+
+
+# menu - inserir e listar
+
+
+
+# lista = [("nome: "), "tlm: ")]
